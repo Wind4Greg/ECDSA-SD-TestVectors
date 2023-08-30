7b6a229b39a529dbf8e54e9644bba9055a7ae289
Wind4Greg <gregb@grotto-networking.com>
2023-08-30 13:40:38 -0700

    Start clean up for add base.

--- SDAddBaseSteps.js
+++ SDAddBaseSteps.js
@@ -2,9 +2,6 @@
     Walking through the steps to  create ("Add") a based SD proof using Digital
     Bazaar functions. The higher level steps are *transformation*, *hashing*, and
     *serialization*.
-
-    For *transformation* `async function canonicalizeAndGroup({document,
-      labelMapFactoryFunction, groups, options})` is used.
 */
 
 import { mkdir, readFile, writeFile } from 'fs/promises';
@@ -19,12 +16,23 @@ import varint from 'varint';
 import { base58btc } from "multiformats/bases/base58";
 import cbor from "cbor";
 import { base64url} from "multiformats/bases/base64";
+// For serialization of JavaScript Map via JSON
+function replacerMap(key, value) { // See https://stackoverflow.com/questions/29085197/how-do-you-json-stringify-an-es6-map
+  if(value instanceof Map) {
+    return {
+      dataType: 'Map',
+      value: Array.from(value.entries()), // or with spread: value: [...value]
+    };
+  } else {
+    return value;
+  }
+}
 
 // Create output directory for the results
 const baseDir = "./output/ecdsa-sd-2023/";
 let status = await mkdir(baseDir, {recursive: true});
 
-// Sample keys
+// Sample long term issuer signing/public keys
 const keyPair = {
   publicKeyMultibase: "zDnaepBuvsQ8cpsWrVKw8fbpGpvPeNSjVPTWoq6cRqaYzBKVP"
 };
@@ -32,7 +40,7 @@ let privateKey = hexToBytes("C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8
 
 jsonld.documentLoader = localLoader; // Local loader for JSON-LD
 
-// Read input document from a file or just specify it right here.
+// Read input document from a file
 let document = JSON.parse(
     await readFile(
       new URL('./input/windDoc.json', import.meta.url)
@@ -48,8 +56,8 @@ let hmacKey = hexToBytes(hmacKeyString);
 const hmac = await createHmac({key: hmacKey});
 const labelMapFactoryFunction = createHmacIdLabelMapFunction({hmac});
 
-// Initialize groupDefinitions to a map with an entry with a key of the string
-// "mandatory" and a value of mandatoryPointers.
+/* Initialize groupDefinitions to a map with an entry with a key of the string
+   "mandatory" and a value of mandatoryPointers. */
 const mandatoryPointers = JSON.parse(
   await readFile(
     new URL('./input/windMandatory.json', import.meta.url)
@@ -67,10 +75,8 @@ const nonMandatory = stuff.groups.mandatory.nonMatching;
 const transformed = {mandatoryPointers, mandatory, nonMandatory, hmacKey};
 // Converting maps to arrays of entries for test vector production not required
 // for algorithm.
-const transformOutput = {mandatoryPointers, mandatory: [...mandatory],
-  nonMandatory: [...nonMandatory], hmacKeyString};
-// console.log(transformOutput);
-await writeFile(baseDir + 'addBaseTransform.json', JSON.stringify(transformOutput, null, 2));
+const transformOutput = {mandatoryPointers, mandatory, nonMandatory, hmacKeyString};
+await writeFile(baseDir + 'addBaseTransform.json', JSON.stringify(transformOutput, replacerMap, 2));
 
 // Missing Step: **Configuration Options**
 // Set proof options per draft
@@ -82,19 +88,18 @@ proofConfig.verificationMethod = "did:key:" + keyPair.publicKeyMultibase + "#" +
 proofConfig.proofPurpose = "assertionMethod";
 proofConfig["@context"] = document["@context"];
 const proofCanon = await jsonld.canonize(proofConfig);
-writeFile(baseDir + 'proofConfigCanonECDSA_SD.txt', proofCanon);
+writeFile(baseDir + 'addProofConfigCanonECDSA_SD.txt', proofCanon);
 
 /* **Hashing Step**
    "The required inputs to this algorithm are a transformed data document (transformedDocument)
    and canonical proof configuration (canonicalProofConfig). A hash data value represented as an
    object is produced as output. " */
 let proofHash = sha256(proofCanon); // @noble/hash will convert string to bytes via UTF-8
-let hashTestVector = {proofHash: bytesToHex(proofHash)};
+
 // 3.3.17 hashMandatoryNQuads
 // Initialize bytes to the UTF-8 representation of the joined mandatory N-Quads.
 // Initialize mandatoryHash to the result of using hasher to hash bytes.
 // Return mandatoryHash.
-
 let mandatoryHash = sha256([...mandatory.values()].join(''));
 // Initialize hashData as a deep copy of transformedDocument and add proofHash as
 // "proofHash" and mandatoryHash as "mandatoryHash" to that object.
@@ -106,8 +111,7 @@ hashData.mandatoryHash = mandatoryHash;
 const hashDataOutput = {};
 hashDataOutput.proofHash = bytesToHex(proofHash);
 hashDataOutput.mandatoryHash = bytesToHex(mandatoryHash);
-// console.log(hashDataOutput);
-writeFile(baseDir + 'hashData.json', JSON.stringify(hashDataOutput, null, 2));
+writeFile(baseDir + 'addHashData.json', JSON.stringify(hashDataOutput, null, 2));
 
 /* 3.5.5 Base Proof Serialization (ecdsa-sd-2023)
   Initialize proofHash, mandatoryPointers, mandatoryHash, nonMandatory, and hmacKey
@@ -132,7 +136,7 @@ nonMandatory.forEach(function(value, key){
 const P256_PUB_PREFIX = 0x1200;
 let p256Prefix = new Uint8Array(varint.encode(P256_PUB_PREFIX)); // Need to use varint on the multicodecs code
 let pub256Encoded = base58btc.encode(concatBytes(p256Prefix, proofPublicKey));
-console.log(`proofPublicKey multikey: ${pub256Encoded}`);
+// console.log(`proofPublicKey multikey: ${pub256Encoded}`);
 
 // 3.4.1 serializeSignData
 //The following algorithm serializes the data that is to be signed by the private key associated
@@ -140,17 +144,14 @@ console.log(`proofPublicKey multikey: ${pub256Encoded}`);
 // the proof-scoped multikey-encoded public key (publicKey), and the mandatory hash (mandatoryHash).
 // A single sign data value, represented as series of bytes, is produced as output.
 // Return the concatenation of proofHash, publicKey, and mandatoryHash, in that order, as sign data.
-let signData = concatBytes(proofHash,base58btc.decode(pub256Encoded), mandatoryHash);
-console.log("mandatory hash:");
-console.log(mandatoryHash);
-console.log(`signData length: ${signData.length}`);
+let signData = concatBytes(proofHash, base58btc.decode(pub256Encoded), mandatoryHash);
 let baseSignature = p256.sign(sha256(signData), privateKey).toCompactRawBytes();
 // baseSignature, publicKey, hmacKey, signatures, and mandatoryPointers are inputs to
 // 3.4.2 serializeBaseProofValue. This seems like a good test vector
 let rawBaseSignatureInfo = { baseSignature: bytesToHex(baseSignature), publicKey: pub256Encoded,
   signatures: signatures.map(sig => bytesToHex(sig)), mandatoryPointers};
 // console.log(rawBaseSignatureInfo);
-writeFile(baseDir + 'rawBaseSignatureInfo.json', JSON.stringify(rawBaseSignatureInfo, null, 2));
+writeFile(baseDir + 'addRawBaseSignatureInfo.json', JSON.stringify(rawBaseSignatureInfo, null, 2));
 
 /* 3.4.2 serializeBaseProofValue
 The following algorithm serializes the base proof value, including the base signature, public key,
@@ -168,9 +169,7 @@ CBOR-encode components and append it to proofValue.
 Initialize baseProof to a string with the multibase-base64url-no-pad-encoding of proofValue. That is, return a
  string starting with "u" and ending with the base64url-no-pad-encoded value of proofValue.
 Return baseProof as base proof.
-
 */
-
 let proofValue = new Uint8Array([0xd9, 0x5d, 0x00]);
 let components = [baseSignature, base58btc.decode(pub256Encoded), hmacKey, signatures, mandatoryPointers];
 let cborThing = await cbor.encodeAsync(components);
@@ -185,4 +184,4 @@ delete proofConfig['@context'];
 signedDocument.proof = proofConfig;
 signedDocument.proof.proofValue = baseProof;
 console.log(JSON.stringify(signedDocument, null, 2));
-writeFile(baseDir + 'signedSDBase.json', JSON.stringify(signedDocument, null, 2));
+writeFile(baseDir + 'addSignedSDBase.json', JSON.stringify(signedDocument, null, 2));
